a0258fda422fb049cfd8d0de66d6b6ffadd5adbc
RealMarioD <35196073+RealMarioD@users.noreply.github.com>
2022-08-23 20:26:24 +0200

    Upload main file

---
+++ ytmplus.user.js
@@ -0,0 +1,365 @@
+// ==UserScript==
+// @name         YTM+
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Gives the YTM site a better look.
+// @author       Mario_D#7052
+// @match        https://music.youtube.com/*
+// @icon         https://imgur.com/gfg6VLJ.png
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    let open = false; // Used to track if config window is open or not
+    let pageDiv; // The player "overlay"
+    let upgradeText;
+    let clockFunc;
+    let GM_config = new GM_configStruct({
+        'id': 'ytmPlusCfg',
+        'title': 'Settings',
+        'fields': {
+            'clock': {
+                'label': 'Change "Upgrade" text to digital clock',
+                'section': 'Utilities',
+                'type': 'checkbox',
+                'default': true
+            },
+            'padding': {
+                'label': 'Change Padding (Refresh for changes)',
+                'type': 'checkbox',
+                'default': true
+            },
+            'visualizer': {
+                'label': 'Enable Music Visualizer (refresh for changes)',
+                'type': 'checkbox',
+                'default': true
+            },
+            'visR': {
+                'label': 'Red',
+                'section': 'Visualizer Colors (Currently Limited)',
+                'type': 'checkbox',
+                'default': true
+            },
+            'visG': {
+                'label': 'Green',
+                'type': 'checkbox',
+                'default': false
+            },
+            'visB': {
+                'label': 'Blue',
+                'type': 'checkbox',
+                'default': true
+            },
+            'bg': {
+                'label': 'Change Background',
+                'section': 'Background Colors',
+                'type': 'checkbox',
+                'default': true
+            },
+            'bgColor': {
+                'label': 'Background Color',
+                'type': 'color',
+                'default': '#AA0000'
+            },
+            'bgEnableGradient': {
+                'label': 'Enable Gradient',
+                'type': 'checkbox',
+                'default': true
+            },
+            'bgGradient': {
+                'label': 'Background Gradient Color',
+                'type': 'color',
+                'default': '#0000AA'
+            },
+            'clockColor': {
+                'label': 'Color',
+                'section': 'Digital Clock',
+                'type': 'color',
+                'default': '#AA3333'
+            },
+            'clockGradient': {
+                'label': 'Enable Gradient',
+                'type': 'checkbox',
+                'default': true
+            },
+            'clockGradientColor': {
+                'label': 'Gradient Color',
+                'type': 'color',
+                'default': '#3333AA'
+            }
+        },
+        'events': {
+            'open': (doc, win, frame) => {
+                // Customizing settings UI
+                frame.style.width = '550px';
+                // frame.style.height = '500px';
+                frame.style.display = 'block';
+                frame.style.margin = 'auto';
+                frame.style.inset = '0';
+                frame.style.boxShadow = '20px 20px 40px rgba(10, 10, 10, 0.8)'
+                frame.style.border = '';
+                frame.style.borderRadius = '25px'
+                doc.querySelectorAll('*').forEach((node) => {
+                    node.style.fontFamily = 'monospace';
+                    node.style.color = 'rgba(0, 204, 0, 0.8)';
+                });
+                doc.body.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+
+
+                let title = doc.getElementById('ytmPlusCfg_header');
+                title.style.fontSize = '69px';
+                title.style.marginBottom = '20px';
+
+                let fieldLabels = doc.getElementsByClassName('field_label');
+                for (let field of fieldLabels) {
+                    field.style.fontSize = '16px'
+                };
+
+                let options = doc.getElementsByClassName('config_var');
+                for (let o of options) {
+                    o.style.textAlign = 'center';
+                }
+
+                let buttons = doc.getElementById('ytmPlusCfg_buttons_holder');
+                buttons.style.textAlign = 'center';
+
+                doc.getElementById('ytmPlusCfg_field_bgColor').type = 'color';
+                doc.getElementById('ytmPlusCfg_field_bgGradient').type = 'color';
+                doc.getElementById('ytmPlusCfg_field_clockColor').type = 'color';
+                doc.getElementById('ytmPlusCfg_field_clockGradientColor').type = 'color';
+
+                open = true;
+            },
+            'close': () => {
+                open = false;
+            },
+            'save': () => {
+                // Updates updateable stuff on save
+                if (GM_config.get('bg') == false) {
+                    document.body.style.backgroundColor = '#000000';
+                    document.body.style.backgroundImage = '';
+                    pageDiv.style.backgroundColor = '#000000';
+                    pageDiv.style.backgroundImage = '';
+                }
+                else {
+                    addFancy(document.body.style, true);
+                    addFancy(pageDiv.style);
+                }
+
+                if (GM_config.get('clock') == true) {
+                    clockEnable();
+                }
+                else clockEnable(true);
+            }
+        }
+    })
+
+    // window.addEventListener('resize', () => fixCover());
+
+    // Code that opens/closes settings UI
+    window.addEventListener('keydown', (ev) => {
+        if (ev.code == 'Backslash' && ev.ctrlKey == true) {
+            if (open === false) {
+                GM_config.open();
+                open = true;
+            }
+            else {
+                GM_config.close();
+                open = false;
+            }
+        }
+    })
+
+    window.addEventListener('load', () => {
+
+        // Setting up CSS and initial stuff for later use
+        upgradeText = document.getElementsByClassName('tab-title style-scope ytmusic-pivot-bar-item-renderer')[3];
+        const animation = `@keyframes gradient {
+			0% {
+				background-position: 0% 50%;
+			}
+
+			100% {
+				background-position: 100% 50%;
+			}
+		}
+        @keyframes effect {
+            from {
+                background-position: 0% center;
+            }
+            to {
+                background-position: 200% center;
+            }
+        }`
+        const node = document.createElement('style');
+        const textNode = document.createTextNode(animation);
+        node.appendChild(textNode);
+        document.head.appendChild(node);
+
+        pageDiv = document.getElementsByClassName("content style-scope ytmusic-player-page")[0];
+        const nbb = document.getElementById('nav-bar-background');
+
+        if (GM_config.get('bg') == true) {
+            addFancy(document.body.style, true);
+            addFancy(pageDiv.style);
+        }
+
+        // Tries to removes weird padding between and above album cover and navbar
+        if (GM_config.get('padding') == true) {
+            pageDiv.style.paddingTop = '0px';
+            const mainPanel = document.getElementById("main-panel");
+            mainPanel.style.marginTop = '8vh';
+            mainPanel.style.marginBottom = '8vh';
+        }
+
+        // Centers album cover OLD
+        /*
+        const player = document.getElementById('player');
+        player.style.marginTop = '1vh';
+        player.style.marginBottom = '1vh';
+        */
+        // fixCover(); // Break the list thingy on the right
+
+        // Removes buttons - YT removed them
+        /*
+        const buttons = document.getElementsByClassName('av-toggle style-scope ytmusic-av-toggle')[0];
+        buttons.remove();
+        */
+
+        // Rewrites the "Upgrade" button at the top to say the time with fancy colors YEP
+        if (GM_config.get('clock') == true) {
+            clockEnable();
+        }
+
+        if (GM_config.get('visualizer') == true) {
+            // Creating a canvas in nav-bar-background as that's the only div where you can create an element without the site breaking lmao
+            nbb.innerHTML = `<canvas id="canvas" style="position: absolute; left: 0; top: 0; width: 100%; height: 100%;"></canvas>`;
+            nbb.style.opacity = 1;
+            getVideo();
+        }
+    })
+
+    // Functions for code
+    /*
+    // Not fully implemented stuff, doesn't work
+    function vh(v) {
+        var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        return (v * h) / 100;
+    }
+
+    function fixCover() {
+        const player = document.getElementsByClassName('style-scope ytmusic-player-page')[1];
+        player.style.marginTop = `${vh(5)}px`;
+        player.style.marginBottom = `${vh(5)}px`;
+    }
+    */
+
+    function clockEnable(off) {
+        if (off == true) {
+            console.log('hello??')
+            clearTimeout(clockFunc);
+            upgradeText.textContent = 'Upgrade'
+        }
+        else {
+            clearTimeout(clockFunc);
+            updTime(upgradeText);
+        }
+        const a = upgradeText.style;
+        a.background = off == true ? '' : `linear-gradient(to right, ${GM_config.get('clockColor')} 0%, ${GM_config.get('clockGradient') == true ? GM_config.get('clockGradientColor') : GM_config.get('clockColor')} 50%, ${GM_config.get('clockColor')} 100%`;
+        a.backgroundSize = off == true ? '' : '200% auto';
+        a.backgroundClip = off == true ? '' : 'text';
+        a.textFillColor = off == true ? '' : 'transparent';
+        a.webkitBackgroundClip = off == true ? '' : 'text';
+        a.webkitTextFillColor = off == true ? '' : 'transparent';
+        a.fontSize = off == true ? '20px' : '50px';
+        a.animation = off == true ? '' : 'effect 2s linear infinite normal';
+    }
+
+    function addFancy(e, overflow) {
+        e.backgroundImage = `linear-gradient(45deg, ${GM_config.get('bgColor')}, ${GM_config.get('bgEnableGradient') == true ? GM_config.get('bgGradient') : GM_config.get('bgColor')})`;
+        e.animation = 'gradient 5s linear infinite alternate';
+        e.backgroundSize = '150% 150%';
+        e.backgroundAttachment = 'fixed';
+        // e.height = '100vh';
+        if (!overflow) e.overflow = 'hidden';
+    }
+
+    let cT;
+    function updTime(uT) {
+        cT = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        uT.textContent = cT;
+        clockFunc = setTimeout(function () {
+            updTime(uT)
+        }, 1000)
+    }
+
+    let v;
+    function getVideo() {
+        v = document.querySelector('video');
+        if (v) {
+            visualizer()
+        }
+        else {
+            console.warn('Warning: Query "video" not found, retrying in 100ms.');
+            setTimeout(() => { getVideo() }, 100);
+        }
+    }
+
+    function visualizer() {
+        var context = new AudioContext();
+        var src = context.createMediaElementSource(v);
+        var analyser = context.createAnalyser();
+
+        var canvas = document.getElementById("canvas");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight / 2;
+        var ctx = canvas.getContext("2d");
+
+        src.connect(analyser);
+        analyser.connect(context.destination);
+
+        analyser.fftSize = 256;
+        analyser.smoothingTimeConstant = 0.5;
+
+        var bufferLength = analyser.frequencyBinCount;
+        console.log(bufferLength);
+
+        var dataArray = new Uint8Array(bufferLength);
+
+        var WIDTH = canvas.width;
+        var HEIGHT = canvas.height;
+
+        var barWidth = (WIDTH / bufferLength) * 2.5;
+        var barHeight;
+        var x = 0;
+
+        function renderFrame() {
+            requestAnimationFrame(renderFrame);
+
+            x = 0;
+
+            analyser.getByteFrequencyData(dataArray);
+
+            ctx.fillStyle = "#000";
+            ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+            for (var i = 0; i < bufferLength; i++) {
+                barHeight = dataArray[i] * 2;
+
+                var r = GM_config.get('visR') == true ? barHeight / 2 : 0;
+                var g = GM_config.get('visG') == true ? barHeight / 2 : 0;
+                var b = GM_config.get('visB') == true ? barHeight / 2 : 0;
+
+                ctx.fillStyle = `rgb(${r}, ${g}, ${b}, 0.5)`; // "rgb(" + r + "," + g + "," + b + ")";
+                ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+                x += barWidth + 1;
+            }
+        }
+        renderFrame();
+    }
+})();
