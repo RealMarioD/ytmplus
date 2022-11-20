bd44221f8f44d7303daf9a1a1b68673c79b0911e
RealMarioD <mariocraft.MC2003@gmail.com>
2022-11-20 14:20:22 +0100

    skip disliked songs

--- ytmplus.user.js
+++ ytmplus.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         YTM+
 // @namespace    http://tampermonkey.net/
-// @version      1.3.0
+// @version      1.4.0
 // @updateURL    https://github.com/RealMarioD/ytmplus/raw/main/ytmplus.user.js
 // @downloadURL  https://github.com/RealMarioD/ytmplus/raw/main/ytmplus.user.js
 // @description  Ever wanted some nice addons for YouTube Music? If yes, you are at the right place.
@@ -17,13 +17,17 @@
 (function() {
     'use strict';
 
+    // Some global variables
     let open = false; // Used to track if config window is open or not
-    let pageDiv; // The player "overlay"
-    let upgradeText;
-    let clockFunc;
-    let noAfk;
-    let noPromo;
-    // 'type': 'color'; just results in a text input, they are later converted to actual color input
+    let pageDiv; // Set to the player "overlay" in window.onload
+    let upgradeText; // Set to the upgrade "button" in window.onload
+    let clockFunction; // Holds the timeout function that updates the digital clock
+    let noAfkFunction; // Holds the anti-afk interval function
+    let noPromoFunction; // Holds the no promotions function
+    let skipFunction; // Holds the skip disliked songs function
+    let dumbFix = 0; // idek what to type here, DOMSubtreeModified fires twice, this helps code run only once lmao
+
+    // 'type': 'color'; just results in a text input, they are later converted to actual color input, see open event
     const GM_config = new GM_configStruct({
         'id': 'ytmPlusCfg',
         'title': 'ytmPlus',
@@ -35,9 +39,14 @@
                 'default': true
             },
             'noPromo': {
-                'label': 'No Promotions ("Try premium for free!")',
+                'label': 'No Promotions',
                 'type': 'checkbox',
                 'default': true
+            },
+            'skipDisliked': {
+                'label': 'Skip Disliked Songs (Beta)',
+                'type': 'checkbox',
+                'default': false
             },
             'padding': {
                 'label': 'Change Padding (Refresh for changes)',
@@ -113,7 +122,7 @@
             // open function is mostly customizing settings UI
             'open': (doc, win, frame) => {
                 frame.style.width = '550px';
-                frame.style.height = '740px';
+                frame.style.height = '780px';
                 frame.style.display = 'block';
                 frame.style.margin = 'auto';
                 frame.style.inset = '0';
@@ -187,6 +196,20 @@
 
                 if(GM_config.get('noPromo') == true) promoEnable();
                 else promoEnable(true);
+
+                if(GM_config.get('skipDisliked') == true) skipDisliked();
+                else skipDisliked(true);
+
+                const saveBtn = document.getElementById('ytmPlusCfg').contentWindow.document.getElementById('ytmPlusCfg_saveBtn');
+                saveBtn.innerText = 'Saved!';
+                setTimeout(() => {
+                    try {
+                        saveBtn.innerText = 'Save';
+                    }
+                    catch{
+                        undefined;
+                    }
+                }, 2000);
             }
         }
     });
@@ -253,20 +276,6 @@
             mainPanel.style.marginBottom = '8vh';
         }
 
-        // Centers album cover OLD
-        /*
-        const player = document.getElementById('player');
-        player.style.marginTop = '1vh';
-        player.style.marginBottom = '1vh';
-        */
-        // fixCover(); // Break the list thingy on the right
-
-        // Removes buttons - YT removed them
-        /*
-        const buttons = document.getElementsByClassName('av-toggle style-scope ytmusic-av-toggle')[0];
-        buttons.remove();
-        */
-
         // Rewrites or removes the "Upgrade" button at the top
         if(GM_config.get('clock') == 'Digital Clock') clockEnable('Digital Clock');
         else if(GM_config.get('clock') == 'Original') clockEnable('Original');
@@ -279,44 +288,32 @@
             getVideo();
         }
 
-        // Cursed settings button
+        if(GM_config.get('skipDisliked') == true) skipDisliked();
+
+        // Adds a settings button on the navbar
         node = document.createElement('iframe');
         node.id = 'ytmPSettings';
         node.src = 'about:blank';
         node.style = 'top: 7px; left: 100px; height: 50px; opacity: 1; overflow: auto; padding: 0px; position: fixed; width: 50px; z-index: 9999; overflow: hidden;';
         document.body.appendChild(node);
         const frameDoc = document.getElementById('ytmPSettings').contentWindow.document;
+        // I just stole the svg to youtube's settings logo so i ended up with this, sorry not sorry
         frameDoc.body.innerHTML = '<svg id="openSettings" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="display: block; width: 100%; height: 100%; fill: white;"><g class="style-scope yt-icon"><path d="M12,9c1.65,0,3,1.35,3,3s-1.35,3-3,3s-3-1.35-3-3S10.35,9,12,9 M12,8c-2.21,0-4,1.79-4,4s1.79,4,4,4s4-1.79,4-4 S14.21,8,12,8L12,8z M13.22,3l0.55,2.2l0.13,0.51l0.5,0.18c0.61,0.23,1.19,0.56,1.72,0.98l0.4,0.32l0.5-0.14l2.17-0.62l1.22,2.11 l-1.63,1.59l-0.37,0.36l0.08,0.51c0.05,0.32,0.08,0.64,0.08,0.98s-0.03,0.66-0.08,0.98l-0.08,0.51l0.37,0.36l1.63,1.59l-1.22,2.11 l-2.17-0.62l-0.5-0.14l-0.4,0.32c-0.53,0.43-1.11,0.76-1.72,0.98l-0.5,0.18l-0.13,0.51L13.22,21h-2.44l-0.55-2.2l-0.13-0.51 l-0.5-0.18C9,17.88,8.42,17.55,7.88,17.12l-0.4-0.32l-0.5,0.14l-2.17,0.62L3.6,15.44l1.63-1.59l0.37-0.36l-0.08-0.51 C5.47,12.66,5.44,12.33,5.44,12s0.03-0.66,0.08-0.98l0.08-0.51l-0.37-0.36L3.6,8.56l1.22-2.11l2.17,0.62l0.5,0.14l0.4-0.32 C8.42,6.45,9,6.12,9.61,5.9l0.5-0.18l0.13-0.51L10.78,3H13.22 M14,2h-4L9.26,4.96c-0.73,0.27-1.4,0.66-2,1.14L4.34,5.27l-2,3.46 l2.19,2.13C4.47,11.23,4.44,11.61,4.44,12s0.03,0.77,0.09,1.14l-2.19,2.13l2,3.46l2.92-0.83c0.6,0.48,1.27,0.87,2,1.14L10,22h4 l0.74-2.96c0.73-0.27,1.4-0.66,2-1.14l2.92,0.83l2-3.46l-2.19-2.13c0.06-0.37,0.09-0.75,0.09-1.14s-0.03-0.77-0.09-1.14l2.19-2.13 l-2-3.46L16.74,6.1c-0.6-0.48-1.27-0.87-2-1.14L14,2L14,2z" class="style-scope yt-icon"></path></g></svg>';
         frameDoc.getElementById('openSettings').addEventListener('click', () => {
             GM_config.open();
         });
     });
 
-    // Functions for code
-    /*
-    // Not fully implemented stuff, doesn't work
-    function vh(v) {
-        var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        return (v * h) / 100;
-    }
-
-    function fixCover() {
-        const player = document.getElementsByClassName('style-scope ytmusic-player-page')[1];
-        player.style.marginTop = `${vh(5)}px`;
-        player.style.marginBottom = `${vh(5)}px`;
-    }
-    */
-
-    // ytmusic-popup-container actually includes right-click menus and other stuff too so this function actually breaks the site
+    // ytmusic-popup-container actually includes right-click menus and other stuff too, might break site, not tested because popups dont come when i need them
     let popup;
     function promoEnable(turnOff) {
-        if(turnOff) clearInterval(noPromo);
+        if(turnOff) clearInterval(noPromoFunction);
         else {
-            clearInterval(noPromo);
-            noPromo = setInterval(() => {
+            clearInterval(noPromoFunction);
+            noPromoFunction = setInterval(() => {
                 popup = document.getElementsByTagName('ytmusic-popup-container');
-                if(popup.length > 0) {
-                    popup[0].remove();
+                if(popup.length > 1) {
+                    popup[1].remove();
                     console.log('YTM+: Removed a promotion.');
                 }
             }, 1000);
@@ -330,10 +327,10 @@
     }
 
     function afkEnable(turnOff) {
-        if(turnOff == true) clearInterval(noAfk);
+        if(turnOff == true) clearInterval(noAfkFunction);
         else {
-            clearInterval(noAfk);
-            noAfk = setInterval(function() {
+            clearInterval(noAfkFunction);
+            noAfkFunction = setInterval(function() {
                 document.dispatchEvent(new KeyboardEvent('keyup', { bubbles: true, cancelable: true, keyCode: 143, which: 143 }));
                 console.log('YTM+: Nudged the page so user is not AFK.');
             }, 60000);
@@ -342,17 +339,17 @@
 
     function clockEnable(mode) {
         if(mode == 'Original') {
-            clearTimeout(clockFunc);
+            clearTimeout(clockFunction);
             upgradeText.textContent = 'Upgrade';
             upgradeText.parentElement.style.margin = '0px 24px';
         }
         else if(mode == 'Digital Clock') {
-            clearTimeout(clockFunc);
+            clearTimeout(clockFunction);
             updateTime(upgradeText);
             upgradeText.parentElement.style.margin = '0px 24px';
         }
         else {
-            clearTimeout(clockFunc);
+            clearTimeout(clockFunction);
             upgradeText.textContent = '';
             upgradeText.parentElement.style.margin = '0px';
         }
@@ -380,7 +377,7 @@
     function updateTime(updateText) {
         currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
         updateText.textContent = currentTime;
-        clockFunc = setTimeout(function() {
+        clockFunction = setTimeout(function() {
             updateTime(updateText);
         }, 1000);
     }
@@ -447,5 +444,24 @@
             }
         }
         renderFrame();
+    }
+
+    function track() {
+        if(dumbFix == 0) {
+            console.log('track f0 dud');
+            return dumbFix++;
+        }
+        clearTimeout(skipFunction);
+        skipFunction = setTimeout(() => {
+            if(document.getElementById('like-button-renderer').children[0].ariaPressed == 'true') document.getElementsByClassName('next-button style-scope ytmusic-player-bar')[0].click();
+        }, 5000);
+        dumbFix = 0;
+    }
+
+    function skipDisliked(turnOff) {
+        const titleHolder = document.getElementsByClassName('title style-scope ytmusic-player-bar')[0];
+        if(turnOff == true) return titleHolder.removeEventListener('DOMSubtreeModified', track, false);
+        titleHolder.removeEventListener('DOMSubtreeModified', track, false);
+        titleHolder.addEventListener('DOMSubtreeModified', track, false);
     }
 })();
