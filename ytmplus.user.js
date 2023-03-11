2740e737b57129ccb8a9478e55fe78a01552077d
sl0thi3 <127168102+sl0thi3@users.noreply.github.com>
2023-03-11 10:37:47 +0100

    firefox support, fixed positioning & upgradeButton  (#4)
    * firefox support
    
    -
    
    * -
    
    * u
    
    * eslint fix
    
    forgot to run eslint --fix, mb

--- ytmplus.user.js
+++ ytmplus.user.js
@@ -388,6 +388,11 @@
                 margin: 0 0 0.5vh;
                 text-align: center;
             }
+            @-moz-document url-prefix() {
+              #cfgHolder {
+               overflow-y: scroll;
+              }
+            }
             #ytmPlusCfg * {
                 font-family: monospace;
                 color: rgba(255, 255, 255, 0.8);
@@ -447,7 +452,7 @@
                     stroke-dashoffset: 25%; stroke-dasharray: 10%; stroke-width: 3;
                 }
                 100% {
-                    fill: rgba(200,0,85,0.25); stroke: rgba(170,0,85,1); 
+                    fill: rgba(200,0,85,0.25); stroke: rgba(170,0,85,1);
                     stroke-dashoffset: -25%; stroke-dasharray: 10%;
                 }
             }
@@ -568,7 +573,7 @@
                 settingsOpen = false;
             },
             save: () => {
-            // Updates updateable stuff on save
+                // Updates updateable stuff on save
                 if(GM_config.get('bg') == false) {
                     document.body.style.backgroundColor = '#000000';
                     document.body.style.backgroundImage = '';
@@ -623,7 +628,7 @@
     });
 
     window.addEventListener('load', () => {
-    // Creating bg movement animation by injecting css into head
+        // Creating bg movement animation by injecting css into head
         const animation = `@keyframes backgroundGradient {
 			0% {
 				background-position: 0% 50%;
@@ -655,7 +660,7 @@
         afkEnable(GM_config.get('noAfk')); // Credit to q1k - https://greasyfork.org/en/users/1262-q1k
 
         if(GM_config.get('bg') == true) {
-        // Remove weird bg gradient div stuff that ytm added early december 2022
+            // Remove weird bg gradient div stuff that ytm added early december 2022
             document.getElementsByTagName('ytmusic-browse-response')[0].children[0].remove();
             document.getElementsByClassName('background-gradient style-scope ytmusic-browse-response')[0].style.backgroundImage = 'none';
 
@@ -671,10 +676,12 @@
             mainPanel.style.marginTop = '8vh';
             mainPanel.style.marginBottom = '8vh';
         }
+        setTimeout(() => {
+            upgradeButton = document.getElementsByClassName('tab-title style-scope ytmusic-pivot-bar-item-renderer')[3];
+            originalUpgradeText = upgradeButton.textContent;
+            clockEnable(GM_config.get('clock'));
+        }, 500);
 
-        upgradeButton = document.getElementsByClassName('tab-title style-scope ytmusic-pivot-bar-item-renderer')[3];
-        originalUpgradeText = upgradeButton.textContent;
-        clockEnable(GM_config.get('clock'));
 
         // Injecting visualizer canvases
         navBarBg.innerHTML = '<canvas id="visualizerNavbarCanvas" style="position: absolute; left: 0; top: 0; width: 100%; height: 100%; pointer-events: none"></canvas>';
@@ -692,21 +699,21 @@
         node.src = 'about:blank';
         node.style = 'top: 7px; left: 100px; height: 50px; opacity: 1; overflow: auto; padding: 0px; position: fixed; width: 50px; z-index: 9999; overflow: hidden;';
         document.body.appendChild(node);
-        const frameDoc = document.getElementById('ytmPSettings').contentWindow.document;
-        // I just stole the svg to youtube's settings logo so i ended up with this, sorry not sorry
-        frameDoc.body.innerHTML = '<svg id="openSettings" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="display: block; width: 100%; height: 100%; fill: white;"><g class="style-scope yt-icon"><path d="M12,9c1.65,0,3,1.35,3,3s-1.35,3-3,3s-3-1.35-3-3S10.35,9,12,9 M12,8c-2.21,0-4,1.79-4,4s1.79,4,4,4s4-1.79,4-4 S14.21,8,12,8L12,8z M13.22,3l0.55,2.2l0.13,0.51l0.5,0.18c0.61,0.23,1.19,0.56,1.72,0.98l0.4,0.32l0.5-0.14l2.17-0.62l1.22,2.11 l-1.63,1.59l-0.37,0.36l0.08,0.51c0.05,0.32,0.08,0.64,0.08,0.98s-0.03,0.66-0.08,0.98l-0.08,0.51l0.37,0.36l1.63,1.59l-1.22,2.11 l-2.17-0.62l-0.5-0.14l-0.4,0.32c-0.53,0.43-1.11,0.76-1.72,0.98l-0.5,0.18l-0.13,0.51L13.22,21h-2.44l-0.55-2.2l-0.13-0.51 l-0.5-0.18C9,17.88,8.42,17.55,7.88,17.12l-0.4-0.32l-0.5,0.14l-2.17,0.62L3.6,15.44l1.63-1.59l0.37-0.36l-0.08-0.51 C5.47,12.66,5.44,12.33,5.44,12s0.03-0.66,0.08-0.98l0.08-0.51l-0.37-0.36L3.6,8.56l1.22-2.11l2.17,0.62l0.5,0.14l0.4-0.32 C8.42,6.45,9,6.12,9.61,5.9l0.5-0.18l0.13-0.51L10.78,3H13.22 M14,2h-4L9.26,4.96c-0.73,0.27-1.4,0.66-2,1.14L4.34,5.27l-2,3.46 l2.19,2.13C4.47,11.23,4.44,11.61,4.44,12s0.03,0.77,0.09,1.14l-2.19,2.13l2,3.46l2.92-0.83c0.6,0.48,1.27,0.87,2,1.14L10,22h4 l0.74-2.96c0.73-0.27,1.4-0.66,2-1.14l2.92,0.83l2-3.46l-2.19-2.13c0.06-0.37,0.09-0.75,0.09-1.14s-0.03-0.77-0.09-1.14l2.19-2.13 l-2-3.46L16.74,6.1c-0.6-0.48-1.27-0.87-2-1.14L14,2L14,2z" class="style-scope yt-icon"></path></g></svg>';
-        frameDoc.getElementById('openSettings').addEventListener('click', () => {
-            if(settingsOpen == false) {
-                GM_config.open();
-                settingsOpen = true;
-            }
-            else {
-                GM_config.close();
-                settingsOpen = false;
-            }
-        });
+        setTimeout(function() {
+            const frameDoc = document.getElementById('ytmPSettings').contentWindow.document;
+            frameDoc.body.innerHTML = '<svg id="openSettings" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="display: block; width: 100%; height: 100%; fill: white;"><g class="style-scope yt-icon"><path d="M12,9c1.65,0,3,1.35,3,3s-1.35,3-3,3s-3-1.35-3-3S10.35,9,12,9 M12,8c-2.21,0-4,1.79-4,4s1.79,4,4,4s4-1.79,4-4 S14.21,8,12,8L12,8z M13.22,3l0.55,2.2l0.13,0.51l0.5,0.18c0.61,0.23,1.19,0.56,1.72,0.98l0.4,0.32l0.5-0.14l2.17-0.62l1.22,2.11 l-1.63,1.59l-0.37,0.36l0.08,0.51c0.05,0.32,0.08,0.64,0.08,0.98s-0.03,0.66-0.08,0.98l-0.08,0.51l0.37,0.36l1.63,1.59l-1.22,2.11 l-2.17-0.62l-0.5-0.14l-0.4,0.32c-0.53,0.43-1.11,0.76-1.72,0.98l-0.5,0.18l-0.13,0.51L13.22,21h-2.44l-0.55-2.2l-0.13-0.51 l-0.5-0.18C9,17.88,8.42,17.55,7.88,17.12l-0.4-0.32l-0.5,0.14l-2.17,0.62L3.6,15.44l1.63-1.59l0.37-0.36l-0.08-0.51 C5.47,12.66,5.44,12.33,5.44,12s0.03-0.66,0.08-0.98l0.08-0.51l-0.37-0.36L3.6,8.56l1.22-2.11l2.17,0.62l0.5,0.14l0.4-0.32 C8.42,6.45,9,6.12,9.61,5.9l0.5-0.18l0.13-0.51L10.78,3H13.22 M14,2h-4L9.26,4.96c-0.73,0.27-1.4,0.66-2,1.14L4.34,5.27l-2,3.46 l2.19,2.13C4.47,11.23,4.44,11.61,4.44,12s0.03,0.77,0.09,1.14l-2.19,2.13l2,3.46l2.92-0.83c0.6,0.48,1.27,0.87,2,1.14L10,22h4 l0.74-2.96c0.73-0.27,1.4-0.66,2-1.14l2.92,0.83l2-3.46l-2.19-2.13c0.06-0.37,0.09-0.75,0.09-1.14s-0.03-0.77-0.09-1.14l2.19-2.13 l-2-3.46L16.74,6.1c-0.6-0.48-1.27-0.87-2-1.14L14,2L14,2z" class="style-scope yt-icon"></path></g></svg>';
+            frameDoc.getElementById('openSettings').addEventListener('click', () => {
+                if(settingsOpen == false) {
+                    GM_config.open();
+                    settingsOpen = true;
+                }
+                else {
+                    GM_config.close();
+                    settingsOpen = false;
+                }
+            });
+        }, 500);
     });
-
     let popup;
     function promoEnable(turnOn) {
         if(!turnOn) clearInterval(noPromoFunction);
