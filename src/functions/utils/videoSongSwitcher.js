9c18f393e1e06a5edc6b4cce9421e46b0352804b
RealMarioD <mariocraft.MC2003@gmail.com>
2023-11-07 08:18:34 +0100

    fix videoSongSwitcher breaking the whole site

--- src/functions/utils/videoSongSwitcher.js
+++ src/functions/utils/videoSongSwitcher.js
@@ -1,21 +1,27 @@
 import { elements } from '../../globals/elements';
 
+let clone;
 export function videoSongSwitcher(turnOn) {
     const avSwitch = document.getElementById('av-id');
     if(!turnOn) {
-        avSwitch.style.display = 'none';
         elements.player.removeAttribute('has-av-switcher');
         elements.playerPage.removeAttribute('has-av-switcher');
+        avSwitch.style.display = 'none';
+        if(clone) clone.style.display = 'none';
         return;
     }
-    avSwitch.style.display = 'block';
     elements.player.setAttribute('has-av-switcher');
     elements.playerPage.setAttribute('has-av-switcher');
+    avSwitch.style.display = 'none';
+    if(clone) return clone.style.display = 'block';
+
     try {
         if(avSwitch.parentNode.tagName == 'YTMUSIC-NAV-BAR') return;
         const navbar = document.getElementsByTagName('ytmusic-nav-bar')[0];
         navbar.children[1].style.justifyContent = 'space-around';
-        navbar.children[1].append(avSwitch);
+        clone = avSwitch.cloneNode(true);
+        navbar.children[1].append(clone);
+        clone.style.display = 'block';
     }
     catch (err) {
         console.error(err);
